549ee8e3c12a29060e4ce035fd351154edc1358f
Lisa Williams <lisawilliams@Lisas-Laptop.local>
2025-07-31 10:10:56 -0400

    further cleanup of code

--- roller_hockey_rinks/roller-hockey-rinks.R
+++ roller_hockey_rinks/roller-hockey-rinks.R
@@ -8,21 +8,20 @@ library(maps)
 setwd("/Users/lisawilliams/code/roller_hockey_rinks")
 getwd()
 
-# The following code is from a tutorial at this address: 
-# https://youtu.be/iai4iXIM7xs?si=1N1jJC1AOeq9-NLk
-
-#let's add a base map of the world
-#world_coordinates <-map_data("world")
-#this also creates a dataframe called world_coordinates with 99K lines
-
 roller_rinks_ma <- rio::import("/Users/lisawilliams/code/roller_hockey_rinks/roller_hockey_rinks.csv")
 
 is.data.frame(roller_rinks_ma)
 
+# In case I want to view the dataframe 
 # View(roller_rinks_ma)
+
+# `found the idea for this approach here -- great for my purposes as it lets 
+# me generate an interactive map with a single line of code 
+# https://map-rfun.library.duke.edu/01_georeference.html`  
 
 roller_rinks_ma_map <- mapview(roller_rinks_ma, xcol = "Longitude", ycol = "Latitude", crs = 4269, grid = FALSE)
 
+# display map 
 roller_rinks_ma_map
 
 # this works 
